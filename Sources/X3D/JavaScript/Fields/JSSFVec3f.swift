4cb12310afb7f7e6cb5ba46d71f88efe8f67e67f
Holger Seelig <holger.seelig@gmail.com>
2020-11-24 14:41:30 +0100

    Added funcs to JSSFVec3f.

--- Sources/X3D/JavaScript/Fields/JSSFVec3f.swift
+++ Sources/X3D/JavaScript/Fields/JSSFVec3f.swift
@@ -18,9 +18,14 @@ import JavaScriptCore
    
    func add (_ vector : JavaScript .SFVec3f) -> JavaScript .SFVec3f
    func cross (_ vector : JavaScript .SFVec3f) -> JavaScript .SFVec3f
+   func distance (_ vector : JavaScript .SFVec3f) -> Float
    func divide (_ scalar : Float) -> JavaScript .SFVec3f
+   func divVec (_ vector : JavaScript .SFVec3f) -> JavaScript .SFVec3f
    func length () -> Float
+   func lerp (_ vector : JavaScript .SFVec3f, _ t : Float) -> JavaScript .SFVec3f
    func multiply (_ scalar : Float) -> JavaScript .SFVec3f
+   func multVec (_ vector : JavaScript .SFVec3f) -> JavaScript .SFVec3f
+   func negate () -> JavaScript .SFVec3f
    func normalize () -> JavaScript .SFVec3f
    func subtract (_ vector : JavaScript .SFVec3f) -> JavaScript .SFVec3f
 
@@ -104,26 +109,51 @@ Object .defineProperty (SFVec3f .prototype, "2", {
          return SFVec3f (object: X3D .SFVec3f (wrappedValue: simd_cross (object .wrappedValue, vector .object .wrappedValue)))
       }
 
+      public final func distance (_ vector : SFVec3f) -> Float
+      {
+         return simd_distance (object .wrappedValue, vector .object .wrappedValue)
+      }
+
       public final func divide (_ scalar : Float) -> SFVec3f
       {
          return SFVec3f (object: X3D .SFVec3f (wrappedValue: object .wrappedValue / scalar))
       }
       
+      public final func divVec (_ vector : SFVec3f) -> SFVec3f
+      {
+         return SFVec3f (object: X3D .SFVec3f (wrappedValue: object .wrappedValue / vector .object .wrappedValue))
+      }
+
       public final func length () -> Float
       {
          return simd_length (object .wrappedValue)
       }
       
+      public final func lerp (_ vector : SFVec3f, _ t : Float) -> SFVec3f
+      {
+         return SFVec3f (object: X3D .SFVec3f (wrappedValue: mix (object .wrappedValue, vector .object .wrappedValue, t: t)))
+      }
+
       public final func multiply (_ scalar : Float) -> SFVec3f
       {
          return SFVec3f (object: X3D .SFVec3f (wrappedValue: object .wrappedValue * scalar))
       }
       
+      public final func multVec (_ vector : SFVec3f) -> SFVec3f
+      {
+         return SFVec3f (object: X3D .SFVec3f (wrappedValue: object .wrappedValue * vector .object .wrappedValue))
+      }
+
+      public final func negate () -> SFVec3f
+      {
+         return SFVec3f (object: X3D .SFVec3f (wrappedValue: -object .wrappedValue))
+      }
+
       public final func normalize () -> SFVec3f
       {
          return SFVec3f (object: X3D .SFVec3f (wrappedValue: simd_normalize (object .wrappedValue)))
       }
-      
+
       public final func subtract (_ vector : SFVec3f) -> SFVec3f
       {
          return SFVec3f (object: X3D .SFVec3f (wrappedValue: object .wrappedValue - vector .object .wrappedValue))

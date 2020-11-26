81e1d827ce1718d30198a57057e2963cc8c7a427
Holger Seelig <holger.seelig@gmail.com>
2020-11-26 13:32:22 +0100

    Code clean up.

--- Sources/X3D/JavaScript/JSValue.swift
+++ Sources/X3D/JavaScript/JSValue.swift
@@ -21,21 +21,23 @@ extension JavaScript
    {
       switch field .getType ()
       {
-         case .SFBool:      return (field as! X3D .SFBool) .wrappedValue
+         case .SFBool:      return (field as! X3D .SFBool)   .wrappedValue
+         case .SFDouble:    return (field as! X3D .SFDouble) .wrappedValue
+         case .SFFloat:     return (field as! X3D .SFFloat)  .wrappedValue
+         case .SFInt32:     return (field as! X3D .SFInt32)  .wrappedValue
+         case .SFString:    return (field as! X3D .SFString) .wrappedValue
+         case .SFTime:      return (field as! X3D .SFTime)   .wrappedValue
+         
          case .SFColor:     return SFColor     (context, object: (field as! X3D .SFColor))
          case .SFColorRGBA: return SFColorRGBA (context, object: (field as! X3D .SFColorRGBA))
-         case .SFDouble:    return (field as! X3D .SFDouble) .wrappedValue
-         case .SFFloat:     return (field as! X3D .SFFloat) .wrappedValue
-         case .SFInt32:     return (field as! X3D .SFInt32) .wrappedValue
-         case .SFRotation:  return SFRotation (context, object: (field as! X3D .SFRotation))
-         case .SFString:    return (field as! X3D .SFString) .wrappedValue
-         case .SFTime:      return (field as! X3D .SFTime) .wrappedValue
-         case .SFVec2d:     return SFVec2d (context, object: (field as! X3D .SFVec2d))
-         case .SFVec2f:     return SFVec2f (context, object: (field as! X3D .SFVec2f))
-         case .SFVec3d:     return SFVec3d (context, object: (field as! X3D .SFVec3d))
-         case .SFVec3f:     return SFVec3f (context, object: (field as! X3D .SFVec3f))
-         case .SFVec4d:     return SFVec4d (context, object: (field as! X3D .SFVec4d))
-         case .SFVec4f:     return SFVec4f (context, object: (field as! X3D .SFVec4f))
+         case .SFRotation:  return SFRotation  (context, object: (field as! X3D .SFRotation))
+         case .SFVec2d:     return SFVec2d     (context, object: (field as! X3D .SFVec2d))
+         case .SFVec2f:     return SFVec2f     (context, object: (field as! X3D .SFVec2f))
+         case .SFVec3d:     return SFVec3d     (context, object: (field as! X3D .SFVec3d))
+         case .SFVec3f:     return SFVec3f     (context, object: (field as! X3D .SFVec3f))
+         case .SFVec4d:     return SFVec4d     (context, object: (field as! X3D .SFVec4d))
+         case .SFVec4f:     return SFVec4f     (context, object: (field as! X3D .SFVec4f))
+         
          default:
             return 0
       }

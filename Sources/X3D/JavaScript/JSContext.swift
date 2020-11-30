d72f6c6c992150a333403055a76787be331ff503
Holger Seelig <holger.seelig@gmail.com>
2020-11-30 23:48:13 +0100

    Code clean up.

--- Sources/X3D/JavaScript/JSContext.swift
+++ Sources/X3D/JavaScript/JSContext.swift
@@ -243,7 +243,7 @@ extension JavaScript
          
          scriptNode .browser! .console .error ("""
 # JavaScript error at line \(lineNumber), \(column):
-# in Script '\(scriptNode .getName ())' url '\(scriptNode .executionContext! .getWorldURL () .absoluteURL .description)'
+# in Script '\(scriptNode .getName ())' in file '\(scriptNode .executionContext! .getWorldURL () .absoluteURL .description)'
 # in method \(stacktrace).
 #
 # \(exception!)

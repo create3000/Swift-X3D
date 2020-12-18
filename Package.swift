b0d3951993eb7047c217050c13bc93879eab6d73
Holger Seelig <holger.seelig@gmail.com>
2020-12-18 19:41:05 +0100

    Reverted changes.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package (
    name: "X3D",
    platforms: [
       .macOS (.v10_15),
-      .iOS (.v13),
    ],
    products: [
       // Products define the executables and libraries a package produces, and make them visible to other packages.

91ebb422ccec1d7397de2a18d21c8b85920f6ddc
Holger Seelig <holger.seelig@gmail.com>
2021-01-19 21:27:07 +0100

    Imported/Exported nodes.

--- Sources/X3D/Execution/X3DExportedNode.swift
+++ Sources/X3D/Execution/X3DExportedNode.swift
@@ -5,7 +5,43 @@
 //  Created by Holger Seelig on 18.01.21.
 //
 
-public final class X3DExportedNode
+public final class X3DExportedNode :
+   X3DObject
 {
+   // Common properties
    
+   internal final override class var typeName : String { "X3DExportedNode" }
+   
+   // Properties
+   
+   public private(set) final var exportedName   : String
+   public private(set) final weak var localNode : X3DNode?
+
+   // Construction
+   
+   internal init (exportedName : String, localNode : X3DNode)
+   {
+      self .exportedName = exportedName
+      self .localNode    = localNode
+   }
+   
+   // Input/Output
+   
+   internal final override func toVRMLStream (_ stream: X3DOutputStream)
+   {
+      guard let localName = stream .getLocalName (localNode) else { return }
+      
+      stream += stream .Indent
+      stream += "EXPORT"
+      stream += stream .Space
+      stream += localName
+
+      if exportedName != localName
+      {
+         stream += stream .Space
+         stream += "AS"
+         stream += stream .Space
+         stream += exportedName
+      }
+   }
 }
--- Sources/X3D/Execution/X3DImportedNode.swift
+++ Sources/X3D/Execution/X3DImportedNode.swift
@@ -5,7 +5,50 @@
 //  Created by Holger Seelig on 18.01.21.
 //
 
-public final class X3DImportedNode
+public final class X3DImportedNode :
+   X3DObject
 {
+   // Common properties
    
+   internal final override class var typeName : String { "X3DImportedNode" }
+   
+   // Properties
+   
+   public private(set) final weak var executionContext : X3DExecutionContext?
+   public private(set) final weak var inlineNode       : Inline?
+   public private(set) final var exportedName          : String
+   public private(set) final var importedName          : String
+   
+   // Construction
+   
+   internal init (executionContext : X3DExecutionContext, inlineNode : Inline, exportedName : String, importedName : String)
+   {
+      self .executionContext = executionContext
+      self .inlineNode       = inlineNode
+      self .exportedName     = exportedName
+      self .importedName     = importedName
+   }
+   
+   // Input/Output
+   
+   internal final override func toVRMLStream (_ stream : X3DOutputStream)
+   {
+      guard let inlineNode = inlineNode,
+            stream .existsNode (inlineNode) else { return }
+      
+      stream += stream .Indent
+      stream += "IMPORT"
+      stream += stream .Space
+      stream += stream .getName (inlineNode)
+      stream += "."
+      stream += exportedName
+
+      if importedName != exportedName
+      {
+         stream += stream .Space
+         stream += "AS"
+         stream += stream .Space
+         stream += importedName
+      }
+   }
 }

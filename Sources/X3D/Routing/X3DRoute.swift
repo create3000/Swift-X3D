e9656eecc1accd11bc849f99ec8845f0958bfdf8
Holger Seelig <holger.seelig@gmail.com>
2021-01-24 18:17:58 +0100

    JSON output routes.

--- Sources/X3D/Routing/X3DRoute.swift
+++ Sources/X3D/Routing/X3DRoute.swift
@@ -93,6 +93,10 @@ public final class X3DRoute :
       guard let sourceNodeName      = stream .getLocalName (sourceNode),
             let destinationNodeName = stream .getLocalName (destinationNode)
       else { return }
+      
+      guard let sourceField      = sourceField,
+            let destinationField = destinationField
+      else { return }
 
       stream += stream .Indent
       stream += "<ROUTE"
@@ -102,9 +106,9 @@ public final class X3DRoute :
       stream += "'"
       stream += stream .Space
       stream += "fromField='"
-      stream += sourceField! .getName () .toXMLString ()
+      stream += sourceField .getName () .toXMLString ()
 
-      if sourceField! .getAccessType () == .inputOutput
+      if sourceField .getAccessType () == .inputOutput
       {
          stream += "_changed"
       }
@@ -117,20 +121,115 @@ public final class X3DRoute :
       stream += stream .Space
       stream += "toField='"
 
-      if destinationField! .getAccessType () == .inputOutput
+      if destinationField .getAccessType () == .inputOutput
       {
          stream += "set_"
       }
 
-      stream += destinationField! .getName () .toXMLString ()
+      stream += destinationField .getName () .toXMLString ()
       stream += "'"
       stream += "/>"
    }
    
+   internal final override func toJSONStream (_ stream : X3DOutputStream)
+   {
+      guard let sourceNodeName      = stream .getLocalName (sourceNode),
+            let destinationNodeName = stream .getLocalName (destinationNode)
+      else { return }
+      
+      guard let sourceField      = sourceField,
+            let destinationField = destinationField
+      else { return }
+
+      stream += "{"
+      stream += stream .TidySpace
+      stream += "\""
+      stream += "ROUTE"
+      stream += "\""
+      stream += ":"
+      stream += stream .TidyBreak
+      stream += stream .IncIndent ()
+      stream += stream .Indent
+      stream += "{"
+      stream += stream .TidyBreak
+      stream += stream .IncIndent ()
+
+      stream += stream .Indent
+      stream += "\""
+      stream += "@fromNode"
+      stream += "\""
+      stream += ":"
+      stream += stream .TidySpace
+      stream += "\""
+      stream += sourceNodeName .toJSONString ()
+      stream += "\""
+      stream += ","
+      stream += stream .TidyBreak
+
+      stream += stream .Indent
+      stream += "\""
+      stream += "@fromField"
+      stream += "\""
+      stream += ":"
+      stream += stream .TidySpace
+      stream += "\""
+      stream += sourceField .getName () .toJSONString ()
+
+      if sourceField .getAccessType () == .inputOutput
+      {
+         stream += "_changed"
+      }
+
+      stream += "\""
+      stream += ","
+      stream += stream .TidyBreak
+
+      stream += stream .Indent
+      stream += "\""
+      stream += "@toNode"
+      stream += "\""
+      stream += ":"
+      stream += stream .TidySpace
+      stream += "\""
+      stream += destinationNodeName .toJSONString ()
+      stream += "\""
+      stream += ","
+      stream += stream .TidyBreak
+
+      stream += stream .Indent
+      stream += "\""
+      stream += "@toField"
+      stream += "\""
+      stream += ":"
+      stream += stream .TidySpace
+      stream += "\""
+      
+      if destinationField .getAccessType () == .inputOutput
+      {
+         stream += "set_"
+      }
+
+      stream += destinationField .getName () .toJSONString ()
+      stream += "\""
+      stream += stream .TidyBreak
+
+      stream += stream .DecIndent ()
+      stream += stream .Indent
+      stream += "}"
+      stream += stream .TidyBreak
+      stream += stream .DecIndent ()
+      stream += stream .Indent
+      stream += "}"
+   }
+
    internal final override func toVRMLStream (_ stream : X3DOutputStream)
    {
       guard let sourceNodeName      = stream .getLocalName (sourceNode),
             let destinationNodeName = stream .getLocalName (destinationNode)
+      else { return }
+
+      guard let sourceField      = sourceField,
+            let destinationField = destinationField
       else { return }
 
       stream += stream .Indent
@@ -138,9 +237,9 @@ public final class X3DRoute :
       stream += " "
       stream += sourceNodeName
       stream += "."
-      stream += sourceField! .getName ()
+      stream += sourceField .getName ()
 
-      if sourceField! .getAccessType () == .inputOutput
+      if sourceField .getAccessType () == .inputOutput
       {
          stream += "_changed"
       }
@@ -151,12 +250,12 @@ public final class X3DRoute :
       stream += destinationNodeName
       stream += "."
 
-      if destinationField! .getAccessType () == .inputOutput
+      if destinationField .getAccessType () == .inputOutput
       {
          stream += "set_"
       }
 
-      stream += destinationField! .getName ()
+      stream += destinationField .getName ()
       stream += stream .Break
    }
 }
